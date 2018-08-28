56274e0ce5e9606e38edc51dce39b5a689c958a2
elizavel <elizavel@yahoo.com>
2018-08-28 20:39:11 +0800

    modify files

---
+++ Party Rock/PartyRockM.swift
@@ -0,0 +1,22 @@
+//
+//  PartyRockM.swift
+//  Party Rock
+//
+//  Created by Eliza Vel Rosario on 22/08/2018.
+//  Copyright © 2018 Eliza Velcom.eliza. All rights reserved.
+//
+
+import Foundation
+
+class PartyRockM {
+    
+    private var _videoTitle: String!
+    private var _videoURL: String!
+    private var _imgURL: String!
+    
+    
+    var videoTitle: String {
+        
+    }
+    
+}
